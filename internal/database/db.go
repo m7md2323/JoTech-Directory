063c52c92941500f3ac7bd543b5815a694f458b9
Mohammad Al Harahsheh <mohammadalharahsheh04@gmail.com>
2026-05-16 20:26:36 +0300

    Initial ReturnCompaniesByQuery() function

--- internal/database/db.go
+++ internal/database/db.go
@@ -20,7 +20,6 @@ func ConnectDatabase() {
 	}
 
 	database_path := os.Getenv("DATABASE_FILE_PATH")
-	//uploads_path := os.Getenv("UPLOADS_FOLDER_PATH")
 	fmt.Println(database_path)
 	database, err := gorm.Open(sqlite.Open(database_path), &gorm.Config{})
 	if err != nil {
@@ -28,18 +27,20 @@ func ConnectDatabase() {
 	}
 
 	database.AutoMigrate(&models.Company{})
-	/*company := models.Company{
-	      Name : "Google",
-	      Location :"Amman",
-	      LocationURL: "https://maps.app.goo.gl/was2LXQvWw7scD7QA",
-	      Size : "Large - International",
-	      Logo : uploads_path + "/Google_logo.png",
-	  }
-	  database.Create(&company)*/
 	DB = database
 }
 
 func ReturnAllCompanies() []models.Company {
+	companies := []models.Company{}
+	result := DB.Find(&companies)
+	if result.RowsAffected == 0 {
+		fmt.Println("No Rows where found! Something went wrong", result.Error)
+		return nil
+	}
+	return companies
+}
+
+func ReturnCompaniesByQuery(query string) []models.Company {
 	companies := []models.Company{}
 	result := DB.Find(&companies)
 	if result.RowsAffected == 0 {
