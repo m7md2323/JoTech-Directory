f537117507395faf37d6e3f0ed03c98898eaae53
Mohammad Al Harahsheh <mohammadalharahsheh04@gmail.com>
2026-05-16 20:24:54 +0300

    Create some routes

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,10 +6,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found or error reading it, using defaults if applicable")
+	}
 	//initialize the SQLite database
 	database.ConnectDatabase()
 
@@ -21,9 +26,12 @@ func main() {
 	//Routes
 
 	mux.HandleFunc("GET /{$}", handlers.Home)
-	mux.HandleFunc("GET /companyProfile", handlers.CompanyProfile)
+	//mux.HandleFunc("GET /company/{id}", handlers.Company)
 	mux.HandleFunc("GET /companies", handlers.Companies)
 	mux.HandleFunc("GET /about", handlers.About)
+	mux.HandleFunc("GET /add_company", handlers.GetAddCompany)
+	mux.HandleFunc("POST /add_company", handlers.PostAddCompany)
+	mux.HandleFunc("GET /contact", handlers.Contact)
 
 	fmt.Println("Listening to port 8080")
 	log.Fatal(http.ListenAndServe(":8080", mux))
