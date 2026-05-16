454551201c3f88cda38ae38da4c5c242ac82892f
Mohammad Al Harahsheh <mohammadalharahsheh04@gmail.com>
2026-05-16 12:04:23 +0300

    GET Handlers for main pages, [Home, About]

---
+++ internal/handlers/getTemplates.go
@@ -0,0 +1,21 @@
+package handlers
+
+import (
+	"net/http"
+	"Jordan-Tech-Companies/web/templates/pages"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+
+	page := pages.Home()
+	page.Render(r.Context(), w)
+	w.WriteHeader(http.StatusOK)
+
+}
+func About(w http.ResponseWriter, r *http.Request) {
+
+	page := pages.About()
+	page.Render(r.Context(), w)
+	w.WriteHeader(http.StatusOK)
+
+}
