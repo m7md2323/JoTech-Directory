2aa825882791c8478dc80650c043af600f38dfeb
Mohammad Khalid <50216824+m7md2323@users.noreply.github.com>
2026-05-20 16:18:00 +0300

    Its simply a helper with objects for TagsList, TypeList, etc... , They are mainly used in .templ files

---
+++ internal/models/helper.go
@@ -0,0 +1,25 @@
+package models
+
+var TagsList = []string{
+	"Software Development", "Web Development", "Mobile Development", "AI", 
+	"Machine Learning", "Deep Learning", "Data Science", "Cloud Computing", 
+	"Blockchain", "Internet Of Things", "Cyber Security", "Robotics", 
+	"Embedded Systems", "Augmented Reality", "Virtual Reality", "Game Development", 
+	"ECommerce", "Health Tech", "FinTech", "EdTech", "AdTech", "MarTech", "Drones", 
+	"Internship Programs", "Data Analysis", "Networking", "Freelance", 
+	"Computer Vision", "Human Resources",
+}
+
+var TypeList = []string{
+	"Private Company", "Governmental Company", "Non-Profit",
+}
+
+var SizeList = []string{
+	"Startup", "Small", "Medium", "Large", "Enterprise", "Multi-National",
+}
+
+var CitiesList = []string{
+	"Amman", "Zarqa", "Irbid", "Mafraq", "Aqaba", 
+	"Madaba", "As-Salt", "Jerash", "Ma'an", "Tafilah", 
+	"Al-Karak", "Ajloun",
+}
