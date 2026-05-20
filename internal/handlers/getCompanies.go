f9901be998769f75e61941d67f6d528ea98f2dfc
Mohammad Khalid <50216824+m7md2323@users.noreply.github.com>
2026-05-20 16:11:51 +0300

    Build the SearchCompanies handler

--- internal/handlers/getCompanies.go
+++ internal/handlers/getCompanies.go
@@ -1,15 +1,35 @@
 package handlers
 
 import (
-	"net/http"
-	"Jordan-Tech-Companies/web/templates/pages"
 	"Jordan-Tech-Companies/internal/database"
+	"Jordan-Tech-Companies/web/templates/pages"
+	"Jordan-Tech-Companies/web/templates/partials"
+	"fmt"
+	"net/http"
 )
 
 func Companies(w http.ResponseWriter, r *http.Request) {
 
-	page := pages.CompaniesList(database.ReturnAllCompanies())
-	page.Render(r.Context(), w)
-	w.WriteHeader(http.StatusOK)
-
+		w.WriteHeader(http.StatusOK)
+		page := pages.Companies(database.ReturnAllCompanies())
+		page.Render(r.Context(), w)
+	
 }
+
+func SearchCompanies(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	companyName := r.FormValue("search")
+	fmt.Println("Received search query:", companyName)
+	w.WriteHeader(http.StatusOK)
+	
+	if companyName == "" {
+		partial := partials.CompaniesList(database.ReturnAllCompanies())
+		partial.Render(r.Context(), w)
+
+	}else{
+		partial:= partials.CompaniesList(database.ReturnCompaniesByName(companyName))
+		partial.Render(r.Context(), w)
+	}
+}
+
+
